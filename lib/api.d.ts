4d4a16f635e231648d3b2a79f9340233155bad87
Norman Breau <norman@normanbreau.com>
2020-04-25 21:14:27 -0300

    EXposed WAPTestTools to public api

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -2,3 +2,6 @@ export { WebAssetPack } from '@wap/runtime';
 export { PluginManager } from '@wap/runtime';
 export { WAPReader } from '@wap/runtime';
 export { Packer } from '@wap/cli';
+import { WAPTestTools } from './WAPTestTools';
+export { WAPTestTools };
+export default WAPTestTools;
--- src/api.ts
+++ src/api.ts
@@ -3,3 +3,7 @@ export {WebAssetPack} from '@wap/runtime';
 export {PluginManager} from '@wap/runtime';
 export {WAPReader} from '@wap/runtime';
 export {Packer} from '@wap/cli';
+
+import {WAPTestTools} from './WAPTestTools';
+export {WAPTestTools};
+export default WAPTestTools;
