556d3dc87b68e3ac89b7f361819f5ead25d41252
Norman Breau <norman@normanbreau.com>
2020-04-28 23:24:27 -0300

    hexdump api

--- lib/TestWAP.d.ts
+++ lib/TestWAP.d.ts
@@ -1,4 +1,5 @@
 import { WebAssetPack } from '@wap/runtime';
 export declare class TestWAP extends WebAssetPack {
     md5(path: string): string;
+    hexdump(path: string): string;
 }
--- src/TestWAP.ts
+++ src/TestWAP.ts
@@ -9,4 +9,26 @@ export class TestWAP extends WebAssetPack {
         hash.update(data);
         return hash.digest('hex');
     }
+
+    public hexdump(path: string): string {
+        let data: Uint8Array = this._getData(path);
+        let output: string = '';
+
+        for (let i: number = 0; i < data.length; i++) {
+            let byte: number = data[i];
+            
+            output += byte.toString(16);
+
+            if (i > 0 && i % 16 === 0) {
+                output += '\n';
+                continue;
+            }
+
+            if (i > 0 && i % 2 === 0) {
+                output += ' ';
+            }
+        }
+
+        return output;
+    }
 }
