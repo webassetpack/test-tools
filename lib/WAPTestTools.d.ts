566b24befd17eb77b59c827320aa54d3f01b729e
Norman Breau <norman@normanbreau.com>
2020-05-06 01:10:26 -0300

    IDefinition refactor

--- lib/WAPTestTools.d.ts
+++ lib/WAPTestTools.d.ts
@@ -1,5 +1,6 @@
 import { IDictionary } from '@totalpave/interfaces';
+import { IDefinition } from '@wap/cli';
 export declare class WAPTestTools {
     private constructor();
-    static compile(definition: IDictionary<string>): Promise<ArrayBuffer>;
+    static compile(definition: IDictionary<IDefinition>): Promise<ArrayBuffer>;
 }
--- src/WAPTestTools.ts
+++ src/WAPTestTools.ts
@@ -1,11 +1,11 @@
 import { IDictionary } from '@totalpave/interfaces';
 import {Stream} from 'stream';
-import {Packer} from '@wap/cli';
+import {Packer, IDefinition} from '@wap/cli';
 
 export class WAPTestTools {
     private constructor() {}
 
-    public static async compile(definition: IDictionary<string>): Promise<ArrayBuffer> {
+    public static async compile(definition: IDictionary<IDefinition>): Promise<ArrayBuffer> {
         return new Promise<ArrayBuffer>((resolve, reject) => {
             let packer: Packer = new Packer();
             packer.pack(definition).then((stream: Stream) => {
